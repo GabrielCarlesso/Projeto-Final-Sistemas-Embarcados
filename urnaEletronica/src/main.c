da0139bb5d219d8d72f426a8d50c638b0b85fd75
GabrielCarlesso <gcarlessox@gmail.com>
2022-07-15 14:17:10 -0300

    Configurando multitarefas

---
+++ urnaEletronica/src/main.c
@@ -0,0 +1,82 @@
+
+#include <asf.h>
+#include "stdint.h"
+#include "multitarefas.h"
+
+/*
+ * Prototipos das tarefas
+ */
+void tarefa_1(void);
+void tarefa_2(void);
+void tarefa_3(void);
+
+
+/*
+ * Configuracao dos tamanhos das pilhas
+ */
+#define TAM_PILHA_1			(TAM_MINIMO_PILHA + 24)
+#define TAM_PILHA_2			(TAM_MINIMO_PILHA + 24)
+#define TAM_PILHA_3			(TAM_MINIMO_PILHA + 24)
+
+/*
+ * Declaracao das pilhas das tarefas
+ */
+uint32_t PILHA_TAREFA_1[TAM_PILHA_1];
+uint32_t PILHA_TAREFA_2[TAM_PILHA_2];
+uint32_t PILHA_TAREFA_3[TAM_PILHA_3];
+uint32_t PILHA_TAREFA_OCIOSA[TAM_PILHA_OCIOSA];
+
+/*
+ * Funcao principal de entrada do sistema
+ */
+int main(void)
+{
+	system_init();
+	
+	/* Criacao das tarefas */
+	/* Parametros: ponteiro, nome, ponteiro da pilha, tamanho da pilha, prioridade da tarefa */
+	
+	CriaTarefa(tarefa_1, "Tarefa 1", PILHA_TAREFA_1, TAM_PILHA_1, 2);
+	
+	CriaTarefa(tarefa_2, "Tarefa 2", PILHA_TAREFA_2, TAM_PILHA_2, 1);
+	
+	/* Cria tarefa ociosa do sistema */
+	CriaTarefa(tarefa_ociosa,"Tarefa ociosa", PILHA_TAREFA_OCIOSA, TAM_PILHA_OCIOSA, 0);
+	
+	/* Configura marca de tempo */
+	ConfiguraMarcaTempo();   
+	
+	/* Inicia sistema multitarefas */
+	IniciaMultitarefas();
+	
+	/* Nunca chega aqui */
+	while (1)
+	{
+	}
+}
+
+/* Tarefas de exemplo que usam funcoes para suspender/continuar as tarefas */
+void tarefa_1(void)
+{
+	volatile uint16_t a = 0;
+	for(;;)
+	{
+		a++;
+		port_pin_set_output_level(LED_0_PIN, LED_0_ACTIVE); /* Liga LED. */
+		TarefaContinua(2);
+	
+	}
+}
+
+void tarefa_2(void)
+{
+	volatile uint16_t b = 0;
+	for(;;)
+	{
+		b++;
+		TarefaSuspende(2);	
+		port_pin_set_output_level(LED_0_PIN, !LED_0_ACTIVE); 	/* Turn LED off. */
+	}
+}
+
+
