9f69c72836ee839126b8a160758e4168b0adf6bf
GabrielCarlesso <gcarlessox@gmail.com>
2022-07-19 15:04:35 -0300

    Definiçãõ das funções e print dos candidatos

--- urnaEletronica/src/main.c
+++ urnaEletronica/src/main.c
@@ -6,9 +6,10 @@
 /*
  * Prototipos das tarefas
  */
-void tarefa_1(void);
-void tarefa_2(void);
-void tarefa_3(void);
+void printOptions(void);
+void readVote(void);
+void encrypt(void);
+void printResults(void);
 
 
 /*
@@ -17,6 +18,8 @@ void tarefa_3(void);
 #define TAM_PILHA_1			(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_2			(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_3			(TAM_MINIMO_PILHA + 24)
+#define TAM_PILHA_4			(TAM_MINIMO_PILHA + 24)
+
 
 /*
  * Declaracao das pilhas das tarefas
@@ -24,6 +27,7 @@ void tarefa_3(void);
 uint32_t PILHA_TAREFA_1[TAM_PILHA_1];
 uint32_t PILHA_TAREFA_2[TAM_PILHA_2];
 uint32_t PILHA_TAREFA_3[TAM_PILHA_3];
+uint32_t PILHA_TAREFA_4[TAM_PILHA_4];
 uint32_t PILHA_TAREFA_OCIOSA[TAM_PILHA_OCIOSA];
 
 /*
@@ -36,9 +40,13 @@ int main(void)
 	/* Criacao das tarefas */
 	/* Parametros: ponteiro, nome, ponteiro da pilha, tamanho da pilha, prioridade da tarefa */
 	
-	CriaTarefa(tarefa_1, "Tarefa 1", PILHA_TAREFA_1, TAM_PILHA_1, 2);
+	CriaTarefa(printOptions, "printOptions", PILHA_TAREFA_1, TAM_PILHA_1, 1);
 	
-	CriaTarefa(tarefa_2, "Tarefa 2", PILHA_TAREFA_2, TAM_PILHA_2, 1);
+	CriaTarefa(readVote, "readVote", PILHA_TAREFA_2, TAM_PILHA_2, 2);
+
+	CriaTarefa(encrypt, "encrypt", PILHA_TAREFA_3, TAM_PILHA_3, 3);
+	
+	CriaTarefa(readVote, "printResults", PILHA_TAREFA_4, TAM_PILHA_4, 4);
 	
 	/* Cria tarefa ociosa do sistema */
 	CriaTarefa(tarefa_ociosa,"Tarefa ociosa", PILHA_TAREFA_OCIOSA, TAM_PILHA_OCIOSA, 0);
@@ -56,27 +64,24 @@ int main(void)
 }
 
 /* Tarefas de exemplo que usam funcoes para suspender/continuar as tarefas */
-void tarefa_1(void)
+
+void printOptions(void)
 {
-	volatile uint16_t a = 0;
-	for(;;)
-	{
-		a++;
-		port_pin_set_output_level(LED_0_PIN, LED_0_ACTIVE); /* Liga LED. */
-		TarefaContinua(2);
 	
-	}
 }
 
-void tarefa_2(void)
+void readVote(void)
 {
-	volatile uint16_t b = 0;
-	for(;;)
-	{
-		b++;
-		TarefaSuspende(2);	
-		port_pin_set_output_level(LED_0_PIN, !LED_0_ACTIVE); 	/* Turn LED off. */
-	}
+	
+}
+
+void encrypt(void)
+{
+	
 }
 
 
+void printResults(void)
+{
+
+}
